88e285603859dddca27fdb569975c843dba70529
blaise <71590378+blazejblaszczak@users.noreply.github.com>
2025-10-31 16:50:14 +0100

    Create main.py

---
+++ gamedev/asteroids/main.py
@@ -0,0 +1,70 @@
+import sys
+import pygame
+from constants import *
+from player import Player, Shot
+from asteroid import Asteroid
+from asteroid_field import AsteroidField
+from shot import Shot
+
+
+def main():
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
+
+    game_on = True
+
+    while game_on:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+
+        time_passed = game_clock.tick(60)
+        dt = time_passed / 1000
+
+    pygame.quit()
+
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
